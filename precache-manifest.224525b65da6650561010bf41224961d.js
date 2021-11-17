a5bdfb64b169bbca86c1004527667d1ea1dec9f5
Lewis Contreras <legiconba4@gmail.com>
2021-11-17 10:11:54 -0500

    Updates

---
+++ precache-manifest.224525b65da6650561010bf41224961d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f10ea44125797707266dcadced9fb74",
+    "url": "/sales-team/index.html"
+  },
+  {
+    "revision": "be9f32fa7cba223b01fd",
+    "url": "/sales-team/static/css/main.42acf6b3.chunk.css"
+  },
+  {
+    "revision": "327db33ef2f24d9e4855",
+    "url": "/sales-team/static/js/2.92ef2867.chunk.js"
+  },
+  {
+    "revision": "6f001b22a9cb1fcb0d750f6c56d216df",
+    "url": "/sales-team/static/js/2.92ef2867.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be9f32fa7cba223b01fd",
+    "url": "/sales-team/static/js/main.eb3ddfc7.chunk.js"
+  },
+  {
+    "revision": "4a5fbb174162f30cdc26",
+    "url": "/sales-team/static/js/runtime-main.b2527a84.js"
+  },
+  {
+    "revision": "5c0c63fb62ec323a10598d7e9de4609e",
+    "url": "/sales-team/static/media/memories.5c0c63fb.png"
+  }
+]);
